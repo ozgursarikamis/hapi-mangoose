c2fc739f1fe62259821c1f979c9d81566e869c54
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-04-02 21:48:17 +0100

    Interface usage

--- src/RouteHandlers/MainRouteHandlers.ts
+++ src/RouteHandlers/MainRouteHandlers.ts
@@ -1,5 +1,9 @@
+import { IMessage } from "../Models/IMessage";
+
 export class MainRouteHandlers {
-    static HelloWorld(): string {
-        return 'Hello World';
+    static HelloWorld(): IMessage {
+        return {
+            message: 'Hello World (*Interface used)..'
+        }
     }
 }
